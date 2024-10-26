a0db3e708970f30d6dec3d08e487841ae66cddf3
Shimizu Yukoh <ys669@zoo.cs.yale.edu>
2024-10-26 14:05:34 -0400

    adding changes to state object

--- MahjongAI/state.py
+++ MahjongAI/state.py
@@ -4,6 +4,7 @@ from MahjongAI.draw import Naki
 
 
 class StateObject:
+
     def __init__(
         self,
         remaining_turns: int,
@@ -11,21 +12,31 @@ class StateObject:
         remaining_tiles: np.ndarray,
         remaining_tiles_pov: np.ndarray,
         sutehai_tensor: np.ndarray,
+        is_menzen: List[int],
+        double_reaches: List[int],
         reaches: List[int],
         melds: List[List[Naki]],
         scores: List[int],
         kyotaku: int,
         honba: int,
         dora: List[int],
+        parent_tensor: np.ndarray,
+        rounds_remaining: List[int],
+        parent_rounds_remaining: List[int],
     ):
         self.remaining_turns = remaining_turns
         self.hand_tensor = hand_tensor
         self.remaining_tiles = remaining_tiles
         self.remaining_tiles_pov = remaining_tiles_pov
         self.sutehai_tensor = sutehai_tensor
+        self.is_menzen = is_menzen
+        self.double_reaches = double_reaches
         self.reaches = reaches
         self.melds = melds
         self.scores = scores
         self.kyotaku = kyotaku
         self.honba = honba
         self.dora = dora
+        self.parent_tensor = parent_tensor
+        self.rounds_remaining = rounds_remaining
+        self.parent_rounds_remaining = parent_rounds_remaining
