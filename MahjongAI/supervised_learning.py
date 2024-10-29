129e662b662a89f13b7bab2932b79f17e625a495
Shimizu Yukoh <ys669@zoo.cs.yale.edu>
2024-10-29 00:10:25 -0400

    training code implemented - still debugging model

--- MahjongAI/supervised_learning.py
+++ MahjongAI/supervised_learning.py
@@ -22,23 +22,7 @@ def train(max_iters: int, verbose: bool = True):
         print(f"iter {iter}")
 
         for tensors_during, tensors_discard, tensors_post in dataloader:
-            if verbose is True:
-                # print the shape of each of the tensors in tensors_during
-                print(
-                    f"tensors_during: shape = Tuple({tensors_during[0].shape}, Tuple({tensors_during[1][0].shape}, {tensors_during[1][1].shape}, {tensors_during[1][2].shape}), {tensors_during[2].shape}, {tensors_during[3].shape})"
-                )
-
-                # print the shape of each of the tensors in tensors_discard
-                print(
-                    f"tensors_discard: shape = Tuple({tensors_discard[0].shape}, Tuple({tensors_discard[1][0].shape}, {tensors_discard[1][1].shape}, {tensors_discard[1][2].shape}), {tensors_discard[2].shape}, {tensors_discard[3].shape})"
-                )
-
-                # print the shape of each of the tensors in tensors_post
-                print(
-                    f"tensors_post: shape = Tuple({tensors_post[0].shape}, Tuple({tensors_post[1][0].shape}, {tensors_post[1][1].shape}, {tensors_post[1][2].shape}), {tensors_post[2].shape}, {tensors_post[3].shape}"
-                )
-
-            # process tensors_during
+            # ----------------- DURING -----------------
             (
                 encoding_tokens_batch,
                 state_obj_tensor_batch,
@@ -51,18 +35,70 @@ def train(max_iters: int, verbose: bool = True):
             action_mask_batch = action_mask_batch.to(device)
             y_tensor = y_tensor.to(device)
 
-            # TODO
+            logits, loss = model(
+                encoding_tokens_batch,
+                state_obj_tensor_batch,
+                action_mask_batch,
+                y_tensor,
+                "during",
+            )
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-    # logits, loss = model(xb, yb)
-    # optimizer.zero_grad()
-    # loss.backward()
-    # optimizer.step()
+            # ----------------- DISCARD -----------------
+            (
+                encoding_tokens_batch,
+                state_obj_tensor_batch,
+                action_mask_batch,
+                y_tensor,
+            ) = tensors_discard
+            encoding_tokens_batch = encoding_tokens_batch.to(device)
+            for state_obj_tensor in state_obj_tensor_batch:
+                state_obj_tensor = state_obj_tensor.to(device)
+            action_mask_batch = action_mask_batch.to(device)
+            y_tensor = y_tensor.to(device)
 
-    # if iter % eval_interval == eval_interval - 1:
-    #     losses = estimate_loss()
-    #     print(
-    #         f"iter {iter} | train loss {losses['train']} | val loss {losses['val']}"
-    #     )
+            logits, loss = model(
+                encoding_tokens_batch,
+                state_obj_tensor_batch,
+                action_mask_batch,
+                y_tensor,
+                "discard",
+            )
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            # ----------------- POST -----------------
+            (
+                encoding_tokens_batch,
+                state_obj_tensor_batch,
+                action_mask_batch,
+                y_tensor,
+            ) = tensors_post
+            encoding_tokens_batch = encoding_tokens_batch.to(device)
+            for state_obj_tensor in state_obj_tensor_batch:
+                state_obj_tensor = state_obj_tensor.to(device)
+            action_mask_batch = action_mask_batch.to(device)
+            y_tensor = y_tensor.to(device)
+
+            logits, loss = model(
+                encoding_tokens_batch,
+                state_obj_tensor_batch,
+                action_mask_batch,
+                y_tensor,
+                "post",
+            )
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if iter % eval_interval == eval_interval - 1:
+                losses = estimate_loss()
+                print(
+                    f"iter {iter} | train loss {losses['train']} | val loss {losses['val']}"
+                )
 
 
 @torch.no_grad()
